d6aedddf7c264339039602684666b2c18ff2aca1
Artur Keerd <arturkeerd@gmail.com>
2024-10-31 13:09:40 +0200

    expressi uuendus

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require('express')
+/*const { fstat } = require('fs') (ei tea miks. vana reliikvia vast) */ 
 const path = require('path')
+const fs = require('node:fs')
+const { mkdirSync } = require('fs')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join (__dirname, 'views'))
 
-app.get('/', (req, res) =>{
-    res.render('index')
-
+app.get('/', (req, res) =>{/*kaldkriips määrab stardipaiga*/
+    fs.readFile('./tasks', 'utf-8', (error, data) => {
+        if(error) {
+            console.error(error)
+            return 
+        }
+        const tasks = data.split('\n')
+        res.render('index', {tasks:tasks})
+    })
 })
-/*app.get('/about', (req, res) =>{/*kaldkriips määrab stardipaiga*/
-    /*console.log(req, url)
-        res.send(path.join (__dirname, 'views'))*/
-    /*
-})
-/*console.log(app)*//*näitab ära appi sisu*/
 
 app.listen(3001, () => {
     console.log('Server started at http://localhost:3001')
